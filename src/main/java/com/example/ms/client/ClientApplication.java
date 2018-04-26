53e134aae35d6783aa6aaddc196cf98a5938ffbf
unknown <wj06_job@163.com>
2018-04-26 11:57:28 +0800

    first commit

---
+++ src/main/java/com/example/ms/client/ClientApplication.java
@@ -0,0 +1,18 @@
+package com.example.ms.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+/**
+ *
+ * @author wangwenchang
+ * @date 2018/4/25 11:07
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+}
