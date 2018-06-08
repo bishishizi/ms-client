2e115c31ecdee171bad6464083f693a6ab2de5d8
shizibaitou <wj06_job@163.com>
2018-06-08 12:01:58 +0800

    添加守护线程demo

--- src/main/java/com/example/ms/client/mvc/controller/ThreadController.java
+++ src/main/java/com/example/ms/client/mvc/controller/ThreadController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -33,26 +35,37 @@ public class ThreadController {
     public void test() {
         ExecutorService poolExecutor = CustomThreadPoolExecutor.getCustomThreadPoolExecutor();
 
+
+
         System.out.println();
     }
 
     @ApiOperation("线程")
     @GetMapping("/daemon")
-    public void daemon() {
+    public void daemon() throws InterruptedException {
         LogUtil.info("系统当前时间：" + new Date());
-
         Calendar calendar = Calendar.getInstance();
         //这个时间点执行任务
         calendar.add(Calendar.SECOND, 2);
         Date date = calendar.getTime();
-        TaskVO task = new TaskVO();
 //        非守护线程,会一直运行
 //        Timer timer = new Timer();
 //        守护线程,没有用户线程活跃就会停止
         Timer timer = new Timer(true);
 //        2秒之后执行,如果没有活跃线程,守护线程就会停止,不会执行线程方法
-//        timer.schedule(task, date);
+//        timer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                LogUtil.info("执行时间 : " + new Date());
+//            }
+//        }, date);
         //2秒之后第一次执行,每隔2秒执行一次
-        timer.schedule(task, date, 2000);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                LogUtil.info("执行时间 : " + new Date());
+            }
+        }, date, 2000);
+//        TimeUnit.SECONDS.sleep(1000);
     }
 }
