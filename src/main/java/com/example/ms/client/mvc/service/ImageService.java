e79d806d4ae2db076416999caaed1849288bc8a1
shizibaitou <wj06_job@163.com>
2018-07-27 18:43:03 +0800

    更新图片压缩

--- src/main/java/com/example/ms/client/mvc/service/ImageService.java
+++ src/main/java/com/example/ms/client/mvc/service/ImageService.java
@@ -13,6 +13,11 @@ import java.io.IOException;
 public class ImageService {
 
     public static void main(String[] args) throws IOException {
-        Thumbnails.of("D:\\image\\6p.jpg").size(1000, 630).outputQuality(1f).toFile("D:\\image\\6.jpg");
+        Thumbnails.of("D:\\image\\1p.jpg").size(1000, 650).outputQuality(1f).toFile("D:\\image\\21.jpg");
+        Thumbnails.of("D:\\image\\2p.jpg").size(1000, 650).outputQuality(1f).toFile("D:\\image\\22.jpg");
+        Thumbnails.of("D:\\image\\3p.jpg").size(1000, 650).outputQuality(1f).toFile("D:\\image\\23.jpg");
+        Thumbnails.of("D:\\image\\4p.jpg").size(1000, 650).outputQuality(1f).toFile("D:\\image\\24.jpg");
+        Thumbnails.of("D:\\image\\4p.jpg").size(1000, 650).outputQuality(1f).toFile("D:\\image\\25.jpg");
+        Thumbnails.of("D:\\image\\6p.jpg").size(1000, 650).outputQuality(1f).toFile("D:\\image\\26.jpg");
     }
 }
